38c22fe643f5cbe2160fcde1658b1425da14622a
Benny Megidish <bennymegk@gmail.com>
2021-01-16 21:25:39 +0200

    (feat) update only on change & delete if removed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
+const readFile = require('fs').promises.readFile;
 const writeFile = require('fs').promises.writeFile;
+const removeFile = require('fs').promises.rm;
 const join = require('path').join;
 const getPlaylists = require('./src/api').getPlaylists;
 const getPlaylistItems = require('./src/api').getPlaylistItems;
@@ -12,24 +14,59 @@ async function workflow() {
         const videoParts = core.getInput('video-parts');             // contentDetails, id, snippet, status
         const maxResults = core.getInput('max-results');             // 0 - 50, default: 5
 
-        const playlists = await getPlaylists(channelId, key, playlistParts, maxResults);
+        const currentPlaylists = await getPlaylists(channelId, key, playlistParts, maxResults);
+        let previousPlaylists;
 
-        if (playlists && playlists.data.items.length > 0) {
-            // save playlist to file
-            writeFile(join('docs', 'toc.json'), JSON.stringify(playlists.data))
-                .then(console.log(`Playlists index file has been updated!`))
+        if (currentPlaylists && currentPlaylists.data.items.length > 0) {
+            // read previous playlists toc
+            await readFile(join('docs', 'toc.json'))
+                .then(toc => prevPlaylists = toc)
                 .catch(error => console.error(error));
 
-            playlists.data.items.forEach(async (playlist) => {
-                const items = await getPlaylistItems(playlist.id, key, videoParts, maxResults);
-                
-                // save playlist items to file
-                if (items) {
-                    writeFile(join('docs', `${playlist.id}.json`), JSON.stringify(items.data))
-                        .then(console.log(`${playlist.id} playlist file has been updated!`))
-                        .catch(error => console.error(error));
-                }
-            });
+            if (currentPlaylists !== previousPlaylists) {
+                // update playlists toc to file
+                await writeFile(join('docs', 'toc.json'), JSON.stringify(currentPlaylists.data))
+                    .then(console.log(`Playlists table of contents file has been ${prevPlaylists ? 'updated' : 'added'}!`))
+                    .catch(error => console.error(error));
+
+                const previousPlaylistsIds = prevPlaylists.data.items.map(playlist => playlist.id);
+                const currentPlaylistsIds = currentPlaylists.data.items.map(playlist => playlist.id);
+
+                const addedPlaylistsIds = currentPlaylistsIds.filter(id => previousPlaylistsIds.includes(id));
+                const updatedPlaylistsIds = previousPlaylistsIds.filter(id => currentPlaylistsIds.includes(id));
+                const removedPlaylistsIds = previousPlaylistsIds.filter(id => currentPlaylistsIds.includes(id));
+
+                addedPlaylistsIds.forEach(async (id) => {
+                    const items = await getPlaylistItems(id, key, videoParts, maxResults);
+                    
+                    // save playlist items to file
+                    if (items) {
+                        writeFile(join('docs', `${id}.json`), JSON.stringify(items.data))
+                            .then(console.log(`${id} playlist file has been added!`))
+                            .catch(error => console.error(error));
+                    }
+                });
+
+                updatedPlaylistsIds.forEach(async (id) => {
+                    const items = await getPlaylistItems(id, key, videoParts, maxResults);
+                    
+                    // update playlist items file
+                    if (items) {
+                        writeFile(join('docs', `${id}.json`), JSON.stringify(items.data))
+                            .then(console.log(`${id} playlist file has been updated!`))
+                            .catch(error => console.error(error));
+                    }
+                });
+
+                removedPlaylistsIds.forEach(async (id) => {
+                    // remove playlist items file
+                    if (items) {
+                        removeFile(join('docs', `${id}.json`))
+                            .then(console.log(`${id} playlist file has been removed!`))
+                            .catch(error => console.error(error));
+                    }
+                });
+            }
         }
     } catch (error) {
         core.setFailed(error.message);
